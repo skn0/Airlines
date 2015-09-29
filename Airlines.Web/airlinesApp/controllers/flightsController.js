1acf51d34a563a4271d41a248650e917002b2253
skn0 <skno88@gmail.com>
2015-09-29 01:42:00 -0500

    has been added angularjs and initial presentation data.

---
+++ Airlines.Web/airlinesApp/controllers/flightsController.js
@@ -0,0 +1,38 @@
+﻿(function (app) {
+
+    var flightsController = function ($scope) {
+
+        $scope.flights =
+            [
+             {
+                 airline: "United",
+                 flightNumber: 207,
+                 startTime: "13:00",
+                 endTime: "15:00",
+                 totalDuration: 2,
+                 price: 500,
+                 date: "12/1/2014",
+                 cities: ["ORD", "LAS"]
+             },
+             {
+                 airline: "United",
+                 flightNumber: 207,
+                 startTime: "13:00",
+                 endTime: "15:00",
+                 totalDuration: 2,
+                 price: 500,
+                 date: "12/1/2014",
+                 cities: ["SFO", "LAS"]
+             }
+
+            ];
+        
+
+    };
+
+
+    app.controller("flightsController", ["$scope",flightsController]);
+
+}
+(angular.module("airlineApp")
+));
